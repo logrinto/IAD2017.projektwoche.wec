7e5031ed4f473609d707ad9320d2eb91a0eeec5c
Lars Mäder <mail@larsmaeder.ch>
2018-02-08 21:51:10 +0100

    New stuff

--- students/lars/assets/js/script.js
+++ students/lars/assets/js/script.js
@@ -59,7 +59,7 @@ var ctx = document.getElementById('Chart-unfall').getContext('2d');
     data: {
         labels: ["2010", "2015", "2020", "2025", "2030"],
         datasets: [{
-            label: "Personenfälle",
+            label: "Tödliche Personenunfälle",
             backgroundColor: 'rgb(0, 192, 192)',
             borderColor: 'rgb(0, 192, 192)',
             data: [15200, 17600, 25000, 35000, 100000],
@@ -78,13 +78,58 @@ var ctx = document.getElementById('Chart-unfall').getContext('2d');
     });
     
     
-$('#btnClick').on('click',function(){
+    // Chart Prognose   
+var ctx = document.getElementById('Chart-prognose').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["2010", "2015", "2020", "2025", "2030", "2035"],
+        datasets: [{
+            label: "Tödliche Personenunfälle",
+            backgroundColor: 'rgb(0, 192, 192)',
+            borderColor: 'rgb(0, 192, 192)',
+            data: [15200, 17600, 25000, 35000, 100000, 0],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        gridLines: {
+            drawOnChartArea: false,
+        },
+        legend: {
+            display: false,
+        }
+    }
+    });
+    
+    
+$('#btnClick').on('change',function(){
     if($('#1').css('display')!='none'){
     $('#2').show().siblings('div').hide();
     }else if($('#2').css('display')!='none'){
         $('#1').show().siblings('div').hide();
     }
 });
-
+    
+var elem = document.querySelector('.js-switch');
+var init = new Switchery(elem, { 
+    color             : '#64bd63'
+  , secondaryColor    : '#dfdfdf'
+  , jackColor         : '#fff'
+  , jackSecondaryColor: null
+  , className         : 'switchery'
+  , disabled          : false
+  , disabledOpacity   : 0.5
+  , speed             : '0.1s'
+  , size              : 'default'
+});
+    
+window.sr = ScrollReveal();
+sr.reveal('.rvl', { duration: 1200, distance: '500px', useDelay: 'always' });
+sr.reveal('.rvl-2', { duration: 1800, distance: '1000px', useDelay: 'always' });
     
 });
