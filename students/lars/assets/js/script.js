b1f54b6121340a2f072b55f7b941a881059e9f8d
Lars Mäder <mail@larsmaeder.ch>
2018-02-08 16:09:33 +0100

    New

--- students/lars/assets/js/script.js
+++ students/lars/assets/js/script.js
@@ -5,5 +5,86 @@ $(window).on('load', function () {
     render: function (data) {
       document.querySelector('.scrollpos').innerHTML = parseInt(data.curTop / window.innerHeight * 100);
     }
-  });
+  });  
+
+// GIF play on hover    
+$(".gif").hover(
+    function()
+    {
+      var src = $(this).attr("src");
+      $(this).attr("src", src.replace(/\.png$/i, ".gif"));
+    },
+    function()
+    {
+      var src = $(this).attr("src");
+      $(this).attr("src", src.replace(/\.gif$/i, ".png"));
+});
+    
+// Chart Geschwindigkeit   
+var ctx = document.getElementById('Chart-kmh').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["2010", "2015", "2020", "2025", "2030"],
+        datasets: [{
+            label: "Geschwindigkeit in km/h",
+            backgroundColor: 'rgb(0, 131, 200)',
+            borderColor: 'rgb(0, 131, 200)',
+            data: [120, 120, 700, 1600, 6000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        gridLines: {
+            drawOnChartArea: false,
+        },
+        legend: {
+            display: false,
+        }
+    }
+        
+    });
+    
+    // Chart Unfall   
+var ctx = document.getElementById('Chart-unfall').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["2010", "2015", "2020", "2025", "2030"],
+        datasets: [{
+            label: "Personenfälle",
+            backgroundColor: 'rgb(0, 192, 192)',
+            borderColor: 'rgb(0, 192, 192)',
+            data: [15200, 17600, 25000, 35000, 100000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        gridLines: {
+            drawOnChartArea: false,
+        },
+        legend: {
+            display: false,
+        }
+    }
+    });
+    
+    
+$('#btnClick').on('click',function(){
+    if($('#1').css('display')!='none'){
+    $('#2').show().siblings('div').hide();
+    }else if($('#2').css('display')!='none'){
+        $('#1').show().siblings('div').hide();
+    }
+});
+
+    
 });
